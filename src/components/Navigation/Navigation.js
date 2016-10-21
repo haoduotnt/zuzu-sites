9a908eb1c3d7c4fafc0b2134ff39d1719da2fc04
Tran Anh Tuan <tuan.tran@fujixerox.co.jp>
2016-10-21 19:01:08 +0900

    Modify navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,22 +7,52 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { setToken } from '../../actions/token';
 import cx from 'classnames';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Navigation.css';
 import Link from '../Link';
 
-function Navigation({ className }) {
-  return (
-    <div className={cx(s.root, className)} role="navigation">
-      <Link className={s.link} to="/login">Log in</Link>
-    </div>
-  );
+class Navigation extends Component {
+  componentDidMount() {
+    console.log(">>>>>>>>>>>>" + this.props.token);
+    this.props.dispatch(
+      setToken('new token from navigation')
+    );
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.forPerson !== this.props.forPerson) {
+      console.log(">>>>>>>>>>>>" + this.props.token);
+    }
+  }
+
+  render() {
+    this.props.dispatch(
+      setToken('new token from navigation')
+    );
+
+    console.log(">>>>>>>>>>>>" + this.props.token);
+    return (
+      <div className={cx(s.root, this.props.className)} role="navigation">
+        <Link className={s.link} to="/login">Log in</Link>
+      </div>
+    );
+  }
 }
 
 Navigation.propTypes = {
   className: PropTypes.string,
+  token: PropTypes.string,
 };
 
-export default withStyles(s)(Navigation);
+function mapStateToProps(state) {
+  console.log(state)
+  return {
+    token: state.token.token,
+  };
+}
+
+export default connect(mapStateToProps)(withStyles(s)(Navigation));

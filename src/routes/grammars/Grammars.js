0b7dee0bbb4d0156a9441be8f9a0e0debf863116
Tran Anh Tuan <tuan.tran@fujixerox.co.jp>
2016-12-22 16:34:38 +0900

    Add autosuggest

--- src/routes/grammars/Grammars.js
+++ src/routes/grammars/Grammars.js
@@ -10,18 +10,119 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import Autosuggest from 'react-autosuggest';
 import s from './Grammar.css';
+
+const people = [
+  {
+    first: 'Charlie',
+    last: 'Brown',
+    twitter: 'dancounsell',
+  },
+  {
+    first: 'Charlotte',
+    last: 'White',
+    twitter: 'mtnmissy',
+  },
+  {
+    first: 'Chloe',
+    last: 'Jones',
+    twitter: 'ladylexy',
+  },
+  {
+    first: 'Cooper',
+    last: 'King',
+    twitter: 'steveodom',
+  },
+];
+
+const theme = {
+  container: s.container,
+  containerOpen: s.containerOpen,
+  input: s.input,
+  suggestionsContainer: s.suggestionsContainer,
+  suggestionsList: s.suggestionsList,
+  suggestion: s.suggestion,
+  suggestionFocused: s.suggestionFocused,
+  sectionContainer: 'react-autosuggest__section-container',
+  sectionTitle: 'react-autosuggest__section-title',
+};
+
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_special_characters
+const escapeRegexCharacters = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+const getSuggestions = value => {
+  const escapedValue = escapeRegexCharacters(value.trim());
+
+  if (escapedValue === '') {
+    return [];
+  }
+
+  const regex = new RegExp(`^${escapedValue}`, 'i');
+
+  return people.filter(person => regex.test(`${person.first} ${person.last}`));
+};
+
+const getSuggestionValue = suggestion => `${suggestion.first} ${suggestion.last}`;
+
+const renderSuggestion = suggestion => (
+  <span className={`${s.suggestionContent} ${s.dancounsell}`}>{`${suggestion.first} ${suggestion.last}`}</span>
+);
 
 class Grammars extends React.Component {
   static propTypes = {
     grammars: PropTypes.arrayOf(PropTypes.object),
   };
 
+  constructor() {
+    super();
+
+    this.state = {
+      value: '',
+      suggestions: [],
+    };
+  }
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue,
+    });
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value),
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: [],
+    });
+  };
+
+
   render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: 'Type \'c\'',
+      value,
+      onChange: this.onChange,
+    };
+
     return (
       <div className={s.root}>
         <div className={s.container}>
           <h1 className={s.title}>React.js News</h1>
+          <Autosuggest // eslint-disable-line react/jsx-no-undef
+            theme={theme}
+            suggestions={suggestions}
+            onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+            onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+            getSuggestionValue={getSuggestionValue}
+            renderSuggestion={renderSuggestion}
+            inputProps={inputProps}
+          />
           <ul className={s.news}>
             {this.props.grammars.map((item, index) => (
               <li key={index} className={s.newsItem}>
--- src/routes/grammars/index.js
+++ src/routes/grammars/index.js
@@ -9,17 +9,39 @@
 
 import React from 'react';
 import Grammars from './Grammars';
+import fetch from '../../core/fetch';
 import Layout from '../../components/Layout';
+import Maintenance from '../../components/Maintenance';
 
 export default {
 
   path: '/japanese/grammars',
 
   async action() {
+    const resp = await fetch('/graphql', {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: '{grammars(page:0){grammars{id,grammar,hiragana,jlpt}page{size,totalPages,totalElements,number}}}',
+      }),
+      credentials: 'include',
+    });
+    const { data } = await resp.json();
+    let component = (
+      <Maintenance />
+    );
+    if (data && data.grammars.grammars) {
+      component = (
+        <Grammars grammars={data.grammars.grammars} page={data.grammars.page} />
+      );
+    }
     return {
       title: 'Japanese Grammar Summary',
       description: 'This summary of some basic Japanese grammar has been derived from the Japanese grammar text books. Let\'s enjoy with these',
-      component: <Layout type="grammar"><Grammars /></Layout>,
+      component: <Layout type="grammar">{component}</Layout>,
     };
   },
 
